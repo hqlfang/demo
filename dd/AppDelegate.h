4d2fc11939751ecdc22a138a7064a399371e9567
gree2 <hqlgree2@gmail.com>
2013-08-26 22:13:41 +0800

    init

---
+++ dd/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  dd
+//
+//  Created by gree2 on 26/8/13.
+//  Copyright (c) 2013 lynn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ dd/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  dd
+//
+//  Created by gree2 on 26/8/13.
+//  Copyright (c) 2013 lynn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
